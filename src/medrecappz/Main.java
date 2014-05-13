6826db32749ddf99875aa4e774c408ec2a4c510a
beeboypralienka <beeboy_pralienka@rocketmail.com>
2014-05-14 01:42:55 +0700

    Inisialisasi Form Utama

--- src/medrecappz/Main.java
+++ src/medrecappz/Main.java
@@ -5,6 +5,14 @@
 
 package medrecappz;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
+import medrecappz.Gui.FrmUtama;
+
 /**
  *
  * @author Fachrul Pralienka BM
@@ -15,7 +23,20 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            UIManager.setLookAndFeel(new NimbusLookAndFeel());
+        } catch (UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+
+            public void run() {
+                FrmUtama fu = new FrmUtama();
+                fu.setExtendedState(fu.getExtendedState() | JFrame.MAXIMIZED_BOTH);
+                fu.setVisible(true);
+            }
+        });
     }
 
 }
